7ffece1fe57fbb92d25a8c78ff46ed6babd237ac
Vathsala <vathsala.sachar@gmail.com>
2025-03-20 10:11:12 +0000

    comment to explain the first commit

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ import socket  # noqa: F401
 
 def main():
 
+    # create a TCP server listening on port 4221
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     server_socket.accept()  # wait for client
 
