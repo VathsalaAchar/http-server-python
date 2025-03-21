0b2536508b51f409d57db394e214958e696a720d
Vathsala <vathsala.sachar@gmail.com>
2025-03-21 14:30:19 +0000

    responds  with 200 status message

--- app/main.py
+++ app/main.py
@@ -5,7 +5,12 @@ def main():
 
     # create a TCP server listening on port 4221
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()  # wait for client
+    conn, addr = server_socket.accept()  # wait for client
+    status, header, resp = b"HTTP/1.1 200 OK", b"", b""
+    http_resp = b"\r\n".join([status, header, resp])
+    with conn:
+        print(f"connecting from {addr}")
+        conn.sendall(http_resp)
 
 
 if __name__ == "__main__":
